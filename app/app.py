711c256ccb20e828915f5fde036dfb60d7cfeead
Jatin More <jatinmore077@gmail.com>
2022-02-23 09:33:34 +0530

    file changes

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import tensorflow as tf
 import keras
 
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
